5021afb1f5bee04b69a5e93e5873ec4a05cb6773
Cam Hendo <camjameshenderson@gmail.com>
2021-01-24 15:47:27 -0800

    dev

--- assets/js/customscript.js
+++ assets/js/customscript.js
@@ -1,10 +1,6 @@
 import LocomotiveScroll from 'locomotive-scroll';
 
-const scroll = new LocomotiveScroll();
-
-
-
-const scroller = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
 });
